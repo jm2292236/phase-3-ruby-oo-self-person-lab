f57d78eb522382378e82139dbeb07510fc288de6
Jaime Montes de Oca <jaime0422@live.com>
2022-07-22 21:47:03 -0700

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,77 @@
+require 'pry'
+
 # your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize name, bank_account = 25, happiness = 8, hygiene = 8
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(new_happiness)
+        if new_happiness > 10
+            new_happiness = 10
+        elsif new_happiness < 0
+            new_happiness = 0
+        end
+
+        @happiness = new_happiness
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene <= 10 ? hygiene < 0 ? 0 : hygiene : 10
+    end
+
+    def happy?
+        happiness > 7
+    end
+
+    def clean?
+        hygiene > 7
+    end
+
+    def get_paid amount
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend friend
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation friend, topic
+        case topic
+        when "politics"
+            # making an array of people
+            # [self, friend].each {|person| person.happiness -= 2}
+
+            self.happiness -= 2
+            friend.happiness -= 2
+
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
